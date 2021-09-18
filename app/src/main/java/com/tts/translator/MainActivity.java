ef0f5aeab4b16adb8e1725622c5cb4698feada96
roncan1 <carugame12_@naver.com>
2021-09-18 22:18:04 +0900

    first commit

--- app/src/main/java/com/tts/translator/MainActivity.java
+++ app/src/main/java/com/tts/translator/MainActivity.java
@@ -1,14 +1,79 @@
 package com.tts.translator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TextToSpeech tts;
+    private Button speak_out;
+    private EditText input_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTts();
+        speak_out = findViewById(R.id.button);
+        input_text = findViewById(R.id.editText);
+
+        speak_out.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP) // LOLLIPOP이상 버전에서만 실행 가능
+            @Override
+            public void onClick(View v){
+                speakOut();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut(){
+        CharSequence text = input_text.getText();
+        // 첫 번째 매개변수: 음성 출력을 할 텍스트
+        // 두 번째 매개변수: 1. TextToSpeech.QUEUE_FLUSH - 진행중인 음성 출력을 끊고 이번 TTS의 음성 출력
+        //                 2. TextToSpeech.QUEUE_ADD - 진행중인 음성 출력이 끝난 후에 이번 TTS의 음성 출력
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
+    }
+
+    @Override
+    public void onDestroy() {
+        if(tts!=null){ // 사용한 TTS객체 제거
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    public void initTts() { // OnInitListener를 통해서 TTS 초기화
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    //사용할 언어를 설정
+                    int result = tts.setLanguage(Locale.KOREA);
+                    //언어 데이터가 없거나 혹은 언어가 지원하지 않으면...
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(MainActivity.this, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        speak_out.setEnabled(true);
+                        //음성 톤
+                        tts.setPitch(0.7f);
+                        //읽는 속도
+                        tts.setSpeechRate(1.2f);
+                    }
+                }
+            }
+        });
     }
 }
