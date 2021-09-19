46acaa6d33c0e7f8bd757f3ab34fcbc4360c8bca
roncan1 <carugame12_@naver.com>
2021-09-19 13:36:22 +0900

    파파고 api를 이용해서 번역기능 완성

--- app/src/main/java/com/tts/translator/MainActivity.java
+++ app/src/main/java/com/tts/translator/MainActivity.java
@@ -1,79 +1,133 @@
 package com.tts.translator;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Locale;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity{
-
-    private TextToSpeech tts;
-    private Button speak_out;
-    private EditText input_text;
+    Button btn_language, btn_translation, btn_share, btn_copy;
+    EditText ET_input;
+    TextView TV_result, TV_language1, TV_language2;
+    int language = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initTts();
-        speak_out = findViewById(R.id.button);
-        input_text = findViewById(R.id.editText);
+        btn_language = (Button) findViewById(R.id.btn_language);
+        btn_translation = (Button) findViewById(R.id.btn_translation);
+        btn_copy = (Button) findViewById(R.id.btn_copy);
+        btn_share = (Button) findViewById(R.id.btn_share);
+        ET_input = (EditText) findViewById(R.id.ET_original);
+        TV_result = (TextView) findViewById(R.id.TV_translation);
+        TV_language1 = (TextView) findViewById(R.id.TV_language1);
+        TV_language2 = (TextView) findViewById(R.id.TV_language2);
 
-        speak_out.setOnClickListener(new View.OnClickListener(){
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP) // LOLLIPOP이상 버전에서만 실행 가능
+        btn_language.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                speakOut();
+            public void onClick(View v) {
+                language = changeLanguage();
             }
         });
-    }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void speakOut(){
-        CharSequence text = input_text.getText();
-        // 첫 번째 매개변수: 음성 출력을 할 텍스트
-        // 두 번째 매개변수: 1. TextToSpeech.QUEUE_FLUSH - 진행중인 음성 출력을 끊고 이번 TTS의 음성 출력
-        //                 2. TextToSpeech.QUEUE_ADD - 진행중인 음성 출력이 끝난 후에 이번 TTS의 음성 출력
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
-    }
-
-    @Override
-    public void onDestroy() {
-        if(tts!=null){ // 사용한 TTS객체 제거
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onDestroy();
-    }
-
-    public void initTts() { // OnInitListener를 통해서 TTS 초기화
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+        btn_copy.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    //사용할 언어를 설정
-                    int result = tts.setLanguage(Locale.KOREA);
-                    //언어 데이터가 없거나 혹은 언어가 지원하지 않으면...
-                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Toast.makeText(MainActivity.this, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show();
-                    } else {
-                        speak_out.setEnabled(true);
-                        //음성 톤
-                        tts.setPitch(0.7f);
-                        //읽는 속도
-                        tts.setSpeechRate(1.2f);
+            public void onClick(View v) {
+                copy(TV_result.getText().toString());
+            }
+        });
+
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                share(TV_result.getText().toString());
+            }
+        });
+
+        btn_translation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    @Override
+                    public void run() {
+                        String word = ET_input.getText().toString();
+                        // Papago는 3번에서 만든 자바 코드이다.
+                        SetPapago papago = new SetPapago();
+                        String resultWord;
+
+                        if(language == 0){
+                            resultWord= papago.getTranslation(word,"ko","en");
+                        }else{
+                            resultWord= papago.getTranslation(word,"en","ko");
+                        }
+
+                        Bundle papagoBundle = new Bundle();
+                        papagoBundle.putString("resultWord",resultWord);
+
+
+                        Message msg = papago_handler.obtainMessage();
+                        msg.setData(papagoBundle);
+                        papago_handler.sendMessage(msg);
+
                     }
-                }
+                }.start();
             }
         });
     }
+
+    @SuppressLint("HandlerLeak")
+    Handler papago_handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String resultWord = bundle.getString("resultWord");
+            TV_result.setText(resultWord);
+        }
+    };
+
+    void copy(String s) {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("translator", s);
+        clipboardManager.setPrimaryClip(clipData);
+
+        //복사가 되었다면 토스트메시지 노출
+        Toast.makeText(getApplicationContext(), "복사되었습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    void share(String s) {
+        Intent Sharing_intent = new Intent(Intent.ACTION_SEND);
+        Sharing_intent.setType(s);
+
+        String Test_Message = "공유할 Text";
+
+        Sharing_intent.putExtra(Intent.EXTRA_TEXT, Test_Message);
+
+        Intent Sharing = Intent.createChooser(Sharing_intent, "공유하기");
+        startActivity(Sharing);
+    }
+
+    public int changeLanguage() {
+        if (language == 0) {
+            TV_language1.setText("영어");
+            TV_language2.setText("한국어");
+            return 1;
+        } else {
+            TV_language1.setText("한국어");
+            TV_language2.setText("영어");
+            return 0;
+        }
+    }
+
 }
